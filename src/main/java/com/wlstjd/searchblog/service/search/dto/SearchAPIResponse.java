678cd64009cb50b52fe15de6a20554fb704b1558
js <wlstjd34@gmail.com>
2023-03-18 21:28:30 +0900

    미사용 dto 정리

--- src/main/java/com/wlstjd/searchblog/service/search/dto/SearchAPIResponse.java
+++
@@ -1,8 +0,0 @@
-package com.wlstjd.searchblog.service.search.dto;
-
-import com.wlstjd.searchblog.service.search.openapi.kakao.dto.Document;
-
-import java.util.List;
-
-public record SearchAPIResponse(Links links, List<Document> documents, int size, int start) {
-}
