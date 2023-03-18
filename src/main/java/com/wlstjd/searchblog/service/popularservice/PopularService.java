107042907533e27984bd0ab5f5ebffacd781dd06
js <wlstjd34@gmail.com>
2023-03-18 14:09:17 +0900

    Search Ranking 결과 정상 반환

--- src/main/java/com/wlstjd/searchblog/service/popularservice/PopularService.java
+++ src/main/java/com/wlstjd/searchblog/service/popularservice/PopularService.java
@@ -3,10 +3,9 @@ package com.wlstjd.searchblog.service.popularservice;
 import com.wlstjd.searchblog.persist.SearchWordEntity;
 import com.wlstjd.searchblog.persist.SearchWordRepo;
 import com.wlstjd.searchblog.service.dto.popular.PopularList;
+import com.wlstjd.searchblog.service.dto.popular.SearchWord;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,7 +15,10 @@ public class PopularService {
 
     public PopularList getLists() {
         List<SearchWordEntity> top10Keywords = searchWordRepo.findTop10ByOrderByTimesDesc();
+        List<SearchWord> popularList = top10Keywords.stream()
+                .map(e -> new SearchWord(e.getKeyword(), e.getTimes()))
+                .toList();
 
-        return new PopularList(new ArrayList<>());
+        return new PopularList(popularList);
     }
 }
