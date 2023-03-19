41494c430ffcc988d5eea49ef22c3773efc38ab0
js <wlstjd34@gmail.com>
2023-03-19 16:13:47 +0900

    BlogOpenApiWrapperKakaoImpl 테스트 코드 추가

--- src/main/java/com/wlstjd/searchblog/service/search/openapi/kakao/BlogOpenApiWrapperKakaoImpl.java
+++ src/main/java/com/wlstjd/searchblog/service/search/openapi/kakao/BlogOpenApiWrapperKakaoImpl.java
@@ -19,26 +19,40 @@ public class BlogOpenApiWrapperKakaoImpl implements BlogOpenApiWrapper {
     private final String API_URL = "https://dapi.kakao.com/v2/search/blog";
     @Override
     public OpenApiResponse search(String keyword, Sorting sorting, Integer page, Integer size) {
+        Map<String, String> header = collectRequestHeader();
+        Map<String, String> requestBody = collectRequestBody(keyword, sorting, page, size);
+
+        String response = blogOpenApi.get(header,"GET", API_URL + makeQuery(requestBody));
+        if (response == null) {
+            throw new RuntimeException("API Response Failed");
+        }
+
+        return makeResponseInstance(response);
+    }
+
+    private static OpenApiResponse makeResponseInstance(String response) {
+        OpenApiResponse result;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            result = objectMapper.readValue(response, OpenApiResponse.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Json Object Mapper Failed");
+        }
+        return result;
+    }
+
+    private static Map<String, String> collectRequestHeader() {
         Map<String, String> header = new HashMap<>();
         header.put("Authorization", "KakaoAK f5aca1c30f55e20e989e8d0475a92956");
+        return header;
+    }
+
+    private static Map<String, String> collectRequestBody(String keyword, Sorting sorting, Integer page, Integer size) {
         Map<String, String> requestBody = new HashMap<>();
         requestBody.put("query", keyword);
         requestBody.put("sort", sorting.getVar());
         requestBody.put("page", page.toString());
         requestBody.put("size", size.toString());
-
-        String response = blogOpenApi.get(header,"GET", API_URL + makeQuery(requestBody));
-        if (response == null) {
-            throw new RuntimeException("API 호출 결과 없음");
-        }
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        OpenApiResponse result;
-        try {
-            result = objectMapper.readValue(response, OpenApiResponse.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        return result;
+        return requestBody;
     }
 }
